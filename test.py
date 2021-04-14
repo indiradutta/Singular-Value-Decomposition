5fe94ba2bdc51b17dfd08c80cd469adab3cd0ae2
Indira Dutta <id8469@srmist.edu.in>
2021-04-14 19:30:58 +0530

    added files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from src.svd import *
+from src.plot import *
+
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image
+
+path = '/media/dog.jpg'
+img = Image.open(path)
+img_grayscale = img.convert('LA')
+plot(img_grayscale)
+imgmat = np.array( list(img_grayscale.getdata(band = 0)), float)
+imgmat.shape = (img_grayscale.size[1], img_grayscale.size[0])
+imgmat = np.matrix(imgmat)
+A = imgmat
+comp_image = svd(A,50)
+plot_svd(comp_image)
